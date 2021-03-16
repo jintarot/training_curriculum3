a281376d378841da09b3e4cd40d41e4ac5767e07
jintaro.tanba <hnnskrhtnbsd2600@icloud.com>
2021-03-16 16:36:08 +0900

    Revert "isuue6"
    This reverts commit f9e6789c84e363cc721ca51a22e64a54776ac2c0.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def getWeek
@@ -31,11 +31,10 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
-      whataday = wdays[Date.today.wday + x]
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month:(@todays_date + x).month, date:(@todays_date+x).day, plans: today_plans ,youbi:whataday}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
